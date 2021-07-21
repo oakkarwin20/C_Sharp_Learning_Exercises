8f535bcbf8bc26644d614dbf1939bc403940fe4a
oakkarwin20 <87596783+oakkarwin20@users.noreply.github.com>
2021-07-21 14:09:20 -0700

    Add files via upload

---
+++ Array_Brackeys.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListsArrays_Brackeys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //when working with arrays, you write ".length" to get the count
+            //when working with lsits, you write ".count" to get the count
+
+            List<string> siblings = new List<string>();
+
+            siblings.Add("oak");
+            siblings.Add("thuta");
+            siblings.Add("nge");
+            siblings.Add("ganda");
+
+            for (int i = 0; i < siblings.Count; i++)
+            {
+                Console.WriteLine(siblings[i]);
+            }
+
+            siblings.Remove("nge");
+            siblings.RemoveAt(1);
+
+            Console.WriteLine("---------------");
+
+            for (int i = 0; i < siblings.Count; i++)
+            {
+                Console.WriteLine(siblings[i]);
+            }
+        }
+    }
+}
