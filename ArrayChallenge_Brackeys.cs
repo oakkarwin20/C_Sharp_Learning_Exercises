4174764595efa786b1fa0399c001d4fd5e83f32b
oakkarwin20 <87596783+oakkarwin20@users.noreply.github.com>
2021-07-21 15:53:50 -0700

    Add files via upload

---
+++ ArrayChallenge_Brackeys.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ArrayChallenge_Brackeys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many siblings do you have?");
+            int siblingCount = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What are the name of your siblings?");
+            string[] siblings = new string[siblingCount];
+
+            for (int i = 0; i < siblings.Length; i++)
+            {
+                siblings[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("---------------------- \n  we are in Alphabetical order");
+
+            Array.Sort(siblings);
+
+            for (int i = 0; i < siblings.Length; i++)
+            {
+                int rank = i + 1;
+                Console.WriteLine($"{rank}.{siblings[i]}"); 
+            }
+        }
+    }
+}
